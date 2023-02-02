36146144d80c1de407e0c011c696efc95f3d3dfc
Lillie McAlister <l.a.mcalister@outlook.com>
2023-02-01 22:06:23 -0800

    React Router Lab

--- bidpro-admin/src/App.js
+++ bidpro-admin/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import HomePage from "./pages/home/index";
+import AABidTypes from "./pages/american-airlines/bidtypes";
+import ASBidTypes from "./pages/alaska-airlines/bidtypes";
+import FABidTypes from "./pages/frontier-airlines/bidtypes";
+import UPBidTypes from "./pages/ups/bidtypes";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/american-airlines/bidtypes" element={<AABidTypes />} />
+        <Route path="/alaska-airlines/bidtypes" element={<ASBidTypes />} />
+        <Route path="/frontier-airlines/bidtypes" element={<FABidTypes />} />
+        <Route path="/ups/bidtypes" element={<UPBidTypes />} />
+      </Routes>
+    </>
   );
 }
 
