9f3f38d441f78f435166c259744427fb7a42a883
Lillie McAlister <l.a.mcalister@outlook.com>
2023-03-07 00:55:48 -0800

    BidPro Home: Jumbotron - Part 2 Lab

--- bidpro-admin/src/components/Administration/Administration.jsx
+++ bidpro-admin/src/components/Administration/Administration.jsx
@@ -5,9 +5,9 @@ import PropTypes from "prop-types";
  * View component that displayes the administration header with a welcome message
  */
 
-export function AdministrationView({ name }) {
+export function Administration({ name }) {
   return (
-    <div className="col-xs-6 col-lg-8 jumbotron pt-0">
+    <div className="col-6 col-lg-8">
       <h1 className="display-3">ADMINISTRATION</h1>
       <p className="lead">
         Hello {name}! A website dedicated to the administration of Airline data.
@@ -16,6 +16,6 @@ export function AdministrationView({ name }) {
   );
 }
 
-AdministrationView.propTypes = {
+Administration.propTypes = {
   name: PropTypes.string,
 };
