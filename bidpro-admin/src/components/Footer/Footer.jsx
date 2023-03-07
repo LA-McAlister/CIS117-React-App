ac22124c7d0e9eeba42a9fabe6047ade637738ac
Lillie McAlister <l.a.mcalister@outlook.com>
2023-03-06 23:14:15 -0800

    BidPro Home - Getting Started Lab

---
+++ bidpro-admin/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * View Component that displayes the footer
+ */
+
+export function Footer() {
+  return (
+    <footer className="container-fluid text-center">
+      <hr />
+      <small>Copyright &copy; 1993-2023 Exception Design Enterprises</small>
+    </footer>
+  );
+}
